8257e8fc2a5b732d6c89fac4c6b782c8a2a23276
Artsiom Anikeyenka <arty.anikey@gmail.com>
2016-08-15 11:05:44 +0300

    initial commit

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -1,35 +1,232 @@
 #define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include <GLFW/glfw3.h> /* includes <vulkan/vulkan.h> */
 
 #include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <vector>
+#include <cstring>
 
-int main() {
-	glfwInit();
+template <typename T>
+class VDeleter {
+public:
+	VDeleter() : VDeleter([](T _) {}) {}
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << extensionCount << " extensions supported" << std::endl;
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+	VDeleter(std::function<void(T, VkAllocationCallbacks*)> deletef) {
+		this->deleter = [=](T obj) { deletef(obj, nullptr); };
 	}
 
-	glfwDestroyWindow(window);
+	VDeleter(const VDeleter<VkInstance>& instance, std::function<void(VkInstance, T, VkAllocationCallbacks*)> deletef) {
+		this->deleter = [&instance, deletef](T obj) { deletef(instance, obj, nullptr); };
+	}
 
-	glfwTerminate();
+	VDeleter(const VDeleter<VkDevice>& device, std::function<void(VkDevice, T, VkAllocationCallbacks*)> deletef) {
+		this->deleter = [&device, deletef](T obj) { deletef(device, obj, nullptr); };
+	}
 
-	return 0;
+	~VDeleter() {
+		cleanup();
+	}
+
+	T* operator &() {
+		cleanup();
+		return &object;
+	}
+
+	operator T() const {
+		return object;
+	}
+
+private:
+	T object{ VK_NULL_HANDLE };
+	std::function<void(T)> deleter;
+
+	void cleanup() {
+		if (object != VK_NULL_HANDLE) {
+			deleter(object);
+		}
+		object = VK_NULL_HANDLE;
+	}
+};
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+	"VK_LAYER_LUNARG_standard_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+class HelloTriangleApplication {
+public:
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+	}
+
+	static VkBool32 debugCallback(
+		VkDebugReportFlagsEXT flags,
+		VkDebugReportObjectTypeEXT objType,
+		uint64_t obj,
+		size_t location,
+		int32_t code,
+		const char* layerPrefix,
+		const char* msg,
+		void* userData) {
+
+		std::cerr << "validation layer: " << msg << std::endl;
+
+		return VK_FALSE;
+	}
+
+private:
+	GLFWwindow* window;
+	VDeleter<VkInstance> instance{ vkDestroyInstance };
+	std::vector<const char*> requiredExtensions;
+
+	void initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // don't create OpenGL context
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // disable resizing
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan() {
+		createInstance();
+		setupDebugCallback();
+	}
+
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+
+	void createInstance() {
+		VkApplicationInfo appInfo = {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		unsigned int glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+		if (result != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+
+		if (enableValidationLayers && !checkValidationLayerSupport()) {
+			throw std::runtime_error("validation layers requested, but not available!");
+		}
+
+		if (!checkExtensionSupport(glfwExtensionCount, glfwExtensions)) {
+			throw std::runtime_error("glfw extensions are not supported!");
+		}
+
+		if (enableValidationLayers) {
+			createInfo.enabledLayerCount = validationLayers.size();
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+
+			requiredExtensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+		} else {
+			createInfo.enabledLayerCount = 0;
+		}
+
+		createInfo.enabledExtensionCount = requiredExtensions.size();
+		createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+	}
+
+	void setupDebugCallback() {
+		if (!enableValidationLayers) return;
+	}
+
+	bool checkValidationLayerSupport() {
+		uint32_t layerCount;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+		for (const char* layerName : validationLayers) {
+			bool layerFound = false;
+
+			for (const auto& layerProperties : availableLayers) {
+				if (strcmp(layerName, layerProperties.layerName) == 0) {
+					layerFound = true;
+					break;
+				}
+			}
+
+			if (!layerFound) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	bool checkExtensionSupport(unsigned int glfwExtensionCount, const char** glfwExtensions) {
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		for (int i = 0; i < glfwExtensionCount; i++) {
+			bool extensionFound = false;
+
+			for (const auto& extension : extensions) {
+				if (strcmp(glfwExtensions[i], extension.extensionName) == 0) {
+					extensionFound = true;
+
+					requiredExtensions.push_back(glfwExtensions[i]);
+
+					break;
+				}
+			}
+
+			if (!extensionFound) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+};
+
+int main() {
+	HelloTriangleApplication app;
+
+	try {
+		app.run();
+	}
+	catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
