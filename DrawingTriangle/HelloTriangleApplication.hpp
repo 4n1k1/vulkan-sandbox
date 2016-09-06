9022c77291daa3c891b519506c7698e28bc59d6f
Artsiom Anikeyenka <arty.anikey@gmail.com>
2016-09-06 14:20:02 +0300

    Ready to draw

--- DrawingTriangle/HelloTriangleApplication.hpp
+++ DrawingTriangle/HelloTriangleApplication.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <cstring>
 #include <set>
+#include <algorithm>
 
 #include "VDeleter.hpp"
 
@@ -95,6 +96,10 @@ class HelloTriangleApplication {
 	QueueFamilyIndices _queueFamilyIndices;
 	VkQueue _graphicsQueue;
 	VkQueue _presentQueue;
+	VDeleter<VkSwapchainKHR> _swapChain{ _device, vkDestroySwapchainKHR };
+	std::vector<VkImage> _swapChainImages;
+	VkFormat _swapChainImageFormat;
+	VkExtent2D _swapChainExtent;
 
 	void _initWindow() {
 		glfwInit();
@@ -111,6 +116,7 @@ class HelloTriangleApplication {
 		this->_createSurface();
 		this->_pickPhysicalDevice();
 		this->_createLogicalDevice();
+		this->_createSwapChain();
 	}
 
 	void _createSurface() {
@@ -232,7 +238,7 @@ class HelloTriangleApplication {
 			}
 
 			if (this->_physicalDevice == nullptr) {
-				throw std::runtime_error("no GPU with same queue family for graphics and present queues!");
+				throw std::runtime_error("failed to find suitable GPU!");
 			}
 		}
 	};
@@ -245,9 +251,16 @@ class HelloTriangleApplication {
 
 		bool extensionsSupported = this->_checkDeviceExtensionSupport(device);
 
+		bool swapChainAdequate = false;
+
+		if (extensionsSupported) {
+			SwapChainSupportDetails swapChainSupport = this->_querySwapChainSupport(device);
+			swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
+		}
+
 		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
 			this->_queueFamilyIndices.isComplete() && this->_queueFamilyIndices.useSameFamily() &&
-			extensionsSupported;
+			extensionsSupported && swapChainAdequate;
 	}
 
 	bool _checkDeviceExtensionSupport(const VkPhysicalDevice& device) {
@@ -414,6 +427,106 @@ class HelloTriangleApplication {
 		}
 
 		return true;
+	}
+
+	SwapChainSupportDetails _querySwapChainSupport(VkPhysicalDevice device) {
+		SwapChainSupportDetails details;
+
+		vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, this->_surface, &(details.capabilities));
+
+		uint32_t formatCount;
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, this->_surface, &formatCount, nullptr);
+
+		if (formatCount != 0) {
+			details.formats.resize(formatCount);
+			vkGetPhysicalDeviceSurfaceFormatsKHR(device, this->_surface, &formatCount, details.formats.data());
+		}
+
+		uint32_t presentModeCount;
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, this->_surface, &presentModeCount, nullptr);
+
+		if (presentModeCount != 0) {
+			details.presentModes.resize(presentModeCount);
+			vkGetPhysicalDeviceSurfacePresentModesKHR(device, this->_surface, &presentModeCount, details.presentModes.data());
+		}
+
+		return details;
+	}
+
+	VkSurfaceFormatKHR _chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+		if (availableFormats.size() == 1 && availableFormats[0].format == VK_FORMAT_UNDEFINED) {
+			return{ VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
+		}
+
+		for (const auto& availableFormat : availableFormats) {
+			if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+				return availableFormat;
+			}
+		}
+
+		return availableFormats[0];
+	}
+
+	VkPresentModeKHR _chooseSwapPresentMode(const std::vector<VkPresentModeKHR> availablePresentModes) {
+		for (const auto& availablePresentMode : availablePresentModes) {
+			if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+				return availablePresentMode;
+			}
+		}
+
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
+	VkExtent2D _chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+		if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+			return capabilities.currentExtent;
+		} else {
+			VkExtent2D actualExtent = { WIDTH, HEIGHT };
+
+			actualExtent.width = std::max(capabilities.minImageExtent.width, std::min(capabilities.maxImageExtent.width, actualExtent.width));
+			actualExtent.height = std::max(capabilities.minImageExtent.height, std::min(capabilities.maxImageExtent.height, actualExtent.height));
+
+			return actualExtent;
+		}
+	}
+
+	void _createSwapChain() {
+		SwapChainSupportDetails swapChainSupport = this->_querySwapChainSupport(this->_physicalDevice);
+
+		VkSurfaceFormatKHR surfaceFormat = this->_chooseSwapSurfaceFormat(swapChainSupport.formats);
+		VkPresentModeKHR presentMode = this->_chooseSwapPresentMode(swapChainSupport.presentModes);
+		VkExtent2D extent = this->_chooseSwapExtent(swapChainSupport.capabilities);
+
+		uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+		if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount) {
+			imageCount = swapChainSupport.capabilities.maxImageCount;
+		}
+
+		VkSwapchainCreateInfoKHR createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		createInfo.surface = this->_surface;
+		createInfo.minImageCount = imageCount;
+		createInfo.imageFormat = surfaceFormat.format;
+		createInfo.imageColorSpace = surfaceFormat.colorSpace;
+		createInfo.imageExtent = extent;
+		createInfo.imageArrayLayers = 1;
+		createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+		createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+		createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+		createInfo.presentMode = presentMode;
+		createInfo.clipped = VK_TRUE;
+
+		if (vkCreateSwapchainKHR(this->_device, &createInfo, nullptr, &(this->_swapChain)) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create swap chain!");
+		}
+
+		vkGetSwapchainImagesKHR(this->_device, this->_swapChain, &imageCount, nullptr);
+		this->_swapChainImages.resize(imageCount);
+		vkGetSwapchainImagesKHR(this->_device, this->_swapChain, &imageCount, this->_swapChainImages.data());
+
+		this->_swapChainImageFormat = surfaceFormat.format;
+		this->_swapChainExtent = extent;
 	}
 
 public:
