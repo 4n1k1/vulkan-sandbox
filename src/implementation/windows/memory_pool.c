383e2e5ae4159db2444d6911cce6e5153f6565de
Artsiom Anikeyenka <arty.anikey@gmail.com>
2016-11-24 17:38:24 +0300

    memory_pool

--- src/implementation/windows/memory_pool.c
+++ src/implementation/windows/memory_pool.c
@@ -1,4 +1,36 @@
+#include <malloc.h>
+#include <stdint.h>
+
+#define WORD_SIZE 4
+#define MEMORY_POOL_SIZE 536870912
+
 struct memory_pool
 {
 	void *memory;
+
+	uint32_t occupied_memory_offset;
+	uint32_t *allocated_sizes;
 };
+
+static struct memory_pool _mempool;
+
+void setup_memory_pool()
+{
+	_mempool.memory = malloc(MEMORY_POOL_SIZE);
+	_mempool.occupied_memory_offset = 0;
+}
+
+void clear_memory_pool()
+{
+	free(_mempool.memory);
+}
+
+void* allocate_memory(unsigned int bytes)
+{
+	if (bytes < WORD_SIZE) bytes = WORD_SIZE;
+
+	
+
+}
+
+void free_memory()
