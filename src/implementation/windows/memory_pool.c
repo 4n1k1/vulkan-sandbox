1437b02f640e6272b42feaecba4f9918e2735395
Artsiom Anikeyenka <arty.anikey@gmail.com>
2016-12-01 19:39:30 +0300

    memory_pool in progress

--- src/implementation/windows/memory_pool.c
+++ src/implementation/windows/memory_pool.c
@@ -11,11 +11,22 @@ struct memory_pool
 {
 	addr_unit *memory;
 
+	/*
+		Occupied memory chunks.
+	*/
 	struct occupied_memory_chunk *o_chunks;
+
+	/*
+		Free memory chunks insied occupied memory.
+		These are basically holes in occupied memory which
+		must be used when possible and are to be
+		eliminated (memory defragmentation) when possible.
+	*/
 	struct free_memory_chunk *f_chunks;
 
-	uint32_t free_chunks_num;
-	uint32_t occupied_chunks_num;
+	uint32_t f_chunks_num;
+	uint32_t o_chunks_num;
+
 	uint32_t occupied_memory_offset;
 };
 
@@ -23,12 +34,14 @@ struct occupied_memory_chunk
 {
 	addr_unit *addr;
 	uint32_t size;
+	struct occupied_memory_chunk *next;
 };
 
 struct free_memory_chunk
 {
 	addr_unit *addr;
 	uint32_t size;
+	struct free_memory_chunk *next;
 };
 
 static struct memory_pool _mempool;
@@ -46,8 +59,12 @@ void setup_memory_pool()
 	);
 
 	_mempool.occupied_memory_offset = 0;
-	_mempool.free_chunks_num = 0;
-	_mempool.occupied_chunks_num = 0;
+
+	_mempool.f_chunks_num = 0;
+	_mempool.o_chunks_num = 0;
+
+	_mempool.f_chunks = NULL;
+	_mempool.o_chunks = NULL;
 }
 
 void clear_memory_pool()
@@ -66,19 +83,84 @@ void* allocate_memory(unsigned int bytes)
 		addr_units_num += 1;
 	}
 
-	if (_mempool.free_chunks_num == 0)
+	if (_mempool.f_chunks_num != 0)
 	{
-		return (void*)(_mempool.memory + _mempool.occupied_memory_offset);
-	}
-	else
-	{
-		for (uint32_t i = 0; i < _mempool.free_chunks_num; i += 1)
+		for (uint32_t i = 0; i < _mempool.f_chunks_num; i += 1)
 		{
+			if (_mempool.f_chunks[i].size < addr_units_num)
+			{
+				addr_unit *target_addr = _mempool.o_chunks[_mempool.o_chunks_num].addr;
 
+				_mempool.o_chunks[_mempool.o_chunks_num].addr = _mempool.f_chunks[i].addr;
+				_mempool.o_chunks[_mempool.o_chunks_num].size = _mempool.f_chunks[i].size;
+
+				_mempool.o_chunks_num += 1;
+
+				_mempool.f_chunks[i].addr += addr_units_num;
+				_mempool.f_chunks[i].size -= addr_units_num;
+
+				return (void *)(target_addr);
+			}
+
+			if (_mempool.f_chunks[i].size == addr_units_num)
+			{
+				addr_unit *target_addr = _mempool.o_chunks[_mempool.o_chunks_num].addr;
+
+				_mempool.o_chunks[_mempool.o_chunks_num].addr = _mempool.f_chunks[i].addr;
+				_mempool.o_chunks[_mempool.o_chunks_num].size = _mempool.f_chunks[i].size;
+
+				_mempool.o_chunks_num += 1;
+				_mempool.f_chunks_num -= 1;
+
+				return (void *)(target_addr);
+			}
 		}
 	}
+
+	addr_unit *target_addr = _mempool.o_chunks[_mempool.o_chunks_num].addr;
+
+	_mempool.o_chunks[_mempool.o_chunks_num].addr = _mempool.memory + _mempool.occupied_memory_offset;
+	_mempool.o_chunks[_mempool.o_chunks_num].size = addr_units_num;
+
+	_mempool.o_chunks_num += 1;
+	_mempool.occupied_memory_offset += addr_units_num;
+
+	return (void*)(target_addr);
 }
 
 void free_memory(void *addr) {
+	addr_unit *target_addr = (addr_unit *)addr;
 
+	for (unsigned int i = 0; i < _mempool.o_chunks_num; i++)
+	{
+		if (_mempool.o_chunks[i].addr == target_addr)
+		{
+			struct occupied_memory_chunk *target_o_chunk = _mempool.o_chunks + i;
+
+			/*
+				If we release chunk of memory which is the latest memory chunk
+				in _mempool.memory array.
+			*/
+			if (target_o_chunk->addr + target_o_chunk->size == _mempool.memory + _mempool.occupied_memory_offset)
+			{
+				_mempool.occupied_memory_offset -= target_o_chunk->size;
+				_mempool.o_chunks_num -= 1;
+
+				for (unsigned int j = 0; j < _mempool.f_chunks_num; j++)
+				{
+					/*
+						If free chunk was before target_o_chunk.
+					*/
+					if (_mempool.f_chunks[j].addr + _mempool.f_chunks[j].size == _mempool.memory + _mempool.occupied_memory_offset)
+					{
+						_mempool.f_chunks_num -= 1;
+					}
+				}
+			}
+			else
+			{
+
+			}
+		}
+	}
 }
